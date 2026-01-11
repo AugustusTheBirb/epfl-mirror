a800395fc53735c92d770b1fe5e17ce503d877a9
unknown <augustasrink@gmail.com>
2026-01-11 12:08:57 +0200

    removed awards

--- src/containers/Homepage/Awardband/awards.tsx
+++ src/containers/Homepage/Awardband/awards.tsx
@@ -1,44 +1,7 @@
 import React from 'react';
 import './awards.css';
 
-const awards = [
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/firstrunner.avif', 
-    label: 'First Runner-Up' 
-  },
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/climate.avif', 
-    label: 'Best Climate Crisis Project' 
-  },
-  { 
-    icon: 'https://static.igem.org/websites/common/2022/icons/awards/top10-color.svg', 
-    label: 'Top 10 Project' 
-  },
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/sustainable-development-impact.svg', 
-    label: 'Best Sustainable Development' 
-  }, 
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/measurement.svg', 
-    label: 'Best Measurement' 
-  },
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/software-tool.svg', 
-    label: 'Best Software Tool' 
-  },
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/wiki.svg', 
-    label: 'Best Wiki' 
-  },
-  { 
-    icon: 'https://static.igem.wiki/teams/5590/awards/presentation.svg', 
-    label: 'Best Presentation' 
-  },
-  { 
-    icon: 'https://static.igem.org/websites/common/2023/icons/medal-flat.svg', 
-    label: 'Gold Medal' 
-  },
-];
+const awards = [];
 
 const AwardsTicker: React.FC = () => {
   // Duplicate awards for seamless infinite scroll
